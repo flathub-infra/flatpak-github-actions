e54124c642872ac690f032b3b48e61caee97ea08
Dark Dragon <darkdragon-001@web.de>
2024-02-22 10:02:05 +0100

    Fix cache to restore cache from correct arch

--- flatpak-builder/dist/index.js
+++ flatpak-builder/dist/index.js
@@ -2,7 +2,7 @@
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 4682:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+/***/ (function(module, __unused_webpack_exports, __nccwpck_require__) {
 
 const core = __nccwpck_require__(504)
 const exec = __nccwpck_require__(4859)
@@ -72,7 +72,7 @@ class Configuration {
     if (!this._cacheKey) {
       try {
         if (!this._manifestHash) { this._manifestHash = (await computeHash(this.manifestPath)).substring(0, 20) }
-        return `flatpak-builder-${this._manifestHash}-${this.arch}`
+        return `flatpak-builder-${this.arch}-${this._manifestHash}`
       } catch (err) {
         core.setFailed(`Fail to create create cache key based on manifest hash: ${err}`)
       }
@@ -334,8 +334,7 @@ const prepareBuild = async (config) => {
       [...CACHE_PATH], // TODO: drop once https://github.com/actions/toolkit/pull/1378 is merged
       `${cacheKey}`,
       [
-        'flatpak-builder-',
-        'flatpak-'
+        `flatpak-builder-${this.arch}-`
       ]
     )
     if (cacheHitKey !== undefined) {
